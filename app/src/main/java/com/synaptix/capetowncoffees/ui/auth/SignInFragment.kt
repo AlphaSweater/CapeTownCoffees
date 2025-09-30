d8fb903464006a521bb68f718c2b656119de3fb9
AlphaSweater <104318840+AlphaSweater@users.noreply.github.com>
2025-09-30 13:38:21 +0200

    feat: Integrate Hilt for Dependency Injection
    This commit integrates Hilt for dependency injection into the application.
    
    - **`CapeTownCoffeesApp.kt`**: Annotated with `@HiltAndroidApp` to enable Hilt in the application class.
    - **`MainActivity.kt`**: Annotated with `@AndroidEntryPoint` to enable Hilt for the main activity.
    - **`SignInFragment.kt`**: Annotated with `@AndroidEntryPoint` to enable Hilt for the sign-in fragment.
    - **`SignUpFragment.kt`**: Annotated with `@AndroidEntryPoint` to enable Hilt for the sign-up fragment.
    - **`app/build.gradle.kts`**:
        - Hilt dependencies were moved to be grouped with other dependency injection-related libraries for better organization. No functional change to dependencies themselves.

--- app/src/main/java/com/synaptix/capetowncoffees/ui/auth/SignInFragment.kt
+++ app/src/main/java/com/synaptix/capetowncoffees/ui/auth/SignInFragment.kt
@@ -8,7 +8,9 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.synaptix.capetowncoffees.R
 import com.synaptix.capetowncoffees.databinding.FragmentSignInBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SignInFragment : Fragment() {
     private var _binding: FragmentSignInBinding? = null
     private val binding get() = _binding!!
